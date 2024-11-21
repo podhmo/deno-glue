1cdea40e28453fc383befb02d333c17cb6e3b211
podhmo <ababjam61+github@gmail.com>
2024-11-22 01:46:44 +0900

    refactor: define main()

--- main.ts
+++ main.ts
@@ -7,30 +7,36 @@ import { denoPlugins } from "jsr:@luca/esbuild-deno-loader";
 import { PathReplacePlugin } from "./esbuild-plugin.ts";
 
 // main
-const args = parseArgs(Deno.args, {
-  name: "mini-bundle",
-  usageText: `${buildUsage({ name: "mini-bundle" })} <filename>...`,
-  description: "外部の依存は可能な限りesm.shの方に任せる bundler",
-  string: ["outdir", "deno-config"],
-  boolean: ["debug"],
-});
+async function main() {
+  const args = parseArgs(Deno.args, {
+    name: "mini-bundle",
+    usageText: `${buildUsage({ name: "mini-bundle" })} <filename>...`,
+    description: "外部の依存は可能な限りesm.shの方に任せる bundler",
+    string: ["outdir", "deno-config"],
+    boolean: ["debug"],
+  });
 
-// TODO: concurrency
-for (const inputFile of args._) {
-  const buildOptions: BuildOptions = {
-    plugins: [PathReplacePlugin({ configPath: args["deno-config"], debug: args.debug }), ...denoPlugins({
-      loader: "native",
-    })],
-    entryPoints: [inputFile],
-    bundle: true,
-    format: "esm",
-  }
-  if (args.outdir !== undefined) {
-    const outFile = pathjoin(args.outdir, basename(inputFile).replace(/\.tsx?$/, ".mjs"));
-    console.error(`[INFO] write to ${outFile}`);
-    buildOptions.outfile = outFile;
-  }
+  // TODO: concurrency
+  for (const inputFile of args._) {
+    const buildOptions: BuildOptions = {
+      plugins: [PathReplacePlugin({ configPath: args["deno-config"], debug: args.debug }), ...denoPlugins({
+        loader: "native",
+      })],
+      entryPoints: [inputFile],
+      bundle: true,
+      format: "esm",
+    }
+    if (args.outdir !== undefined) {
+      const outFile = pathjoin(args.outdir, basename(inputFile).replace(/\.tsx?$/, ".mjs"));
+      console.error(`[INFO] write to ${outFile}`);
+      buildOptions.outfile = outFile;
+    }
 
-  await esbuild.build(buildOptions);
+    await esbuild.build(buildOptions);
+  }
+  await esbuild.stop();
 }
-await esbuild.stop();
+
+if (import.meta.main) {
+  await main();
+}
