0cbe6aa74ab259784eacce1e18832cb212922b59
podhmo <ababjam61+github@gmail.com>
2024-11-22 01:59:27 +0900

    feat: guess deno.json

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-import { basename, join as pathjoin } from "@std/path"
+import { basename, dirname, join as pathjoin } from "@std/path"
+import { exists } from "@std/fs";
+
 import { buildUsage, parseArgs } from "@podhmo/with-help";
 
 import * as esbuild from "npm:esbuild";
@@ -16,8 +18,20 @@ async function main() {
     boolean: ["debug"],
   });
 
+
   // TODO: concurrency
   for (const inputFile of args._) {
+    let configPath = args["deno-config"];
+    if (configPath === undefined) {
+      const guessedPath = await findClosestConfigFile(dirname(inputFile), ["deno.json"]);
+      if(guessedPath !== null) {
+        if(args.debug) {
+          console.error(`[DEBUG] guessed deno.json is ${guessedPath}`);
+        }
+        configPath = guessedPath;
+      }
+    }
+
     const buildOptions: BuildOptions = {
       plugins: [PathReplacePlugin({ configPath: args["deno-config"], debug: args.debug }), ...denoPlugins({
         loader: "native",
@@ -35,6 +49,25 @@ async function main() {
     await esbuild.build(buildOptions);
   }
   await esbuild.stop();
+}
+
+async function findClosestConfigFile(startPath: string, targetFiles: string[]): Promise<string | null> {
+  let currentPath = await Deno.realPath(startPath);
+
+  while (currentPath) {
+    for (const file of targetFiles) {
+      const filePath = pathjoin(currentPath, file);
+      if (await exists(filePath)) {
+        return filePath;
+      }
+    }
+
+    const parentPath = dirname(currentPath);
+    if (parentPath === currentPath) break;
+    currentPath = parentPath;
+  }
+
+  return null;
 }
 
 if (import.meta.main) {
