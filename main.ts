b132026f8105f52e4b04e4012077d0cf7e791069
podhmo <ababjam61+github@gmail.com>
2024-11-22 01:17:21 +0900

    feat: use esbuild

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import * as esbuild from "npm:esbuild";
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader";
+import { type BuildOptions } from "npm:esbuild";
 import { buildUsage, parseArgs } from "jsr:@podhmo/with-help@0.5.0";
+import { basename, join as pathjoin } from "jsr:@std/path@1.0.8"
 
 
 function PathReplacePlugin(options: { configPath?: string } = {}) {
@@ -109,27 +111,34 @@ function PathReplacePlugin(options: { configPath?: string } = {}) {
 // main
 const args = parseArgs(Deno.args, {
   name: "mini-bundle",
-  usageText: `${buildUsage({ name: "bundle" })} <filename>...`,
+  usageText: `${buildUsage({ name: "mini-bundle" })} <filename>...`,
   description: "外部の依存は可能な限りesm.shの方に任せる bundler",
-  string: ["output", "deno-config"],
+  string: ["outdir", "deno-config"],
   boolean: ["debug"],
 });
+
 function debug(msg: string) {
   if (args.debug) {
     console.error(msg);
   }
 }
 
-for (const input of args._) {
-  // TODO: concurrency
-  await esbuild.build({
-    plugins: [PathReplacePlugin({ configPath: args["deno-config"]}), ...denoPlugins({
+// TODO: concurrency
+for (const inputFile of args._) {
+  const buildOptions: BuildOptions = {
+    plugins: [PathReplacePlugin({ configPath: args["deno-config"] }), ...denoPlugins({
       loader: "native",
     })],
-    entryPoints: [input],
-    outfile: args.output,
+    entryPoints: [inputFile],
     bundle: true,
     format: "esm",
-  })
-  await esbuild.stop();
+  }
+  if (args.outdir !== undefined) {
+    const outFile = pathjoin(args.outdir, basename(inputFile).replace(/\.tsx?$/, ".mjs"));
+    console.error(`[INFO] write to ${outFile}`);
+    buildOptions.outfile = outFile;
+  }
+
+  await esbuild.build(buildOptions);
 }
+await esbuild.stop();
