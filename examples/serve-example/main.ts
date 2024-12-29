6c7d0d128b08a659f6f23e02ec4290931620763f
podhmo <ababjam61+github@gmail.com>
2024-12-29 22:09:41 +0900

    chore: update example

--- examples/serve-example/main.ts
+++ examples/serve-example/main.ts
@@ -1,9 +1,15 @@
 import { type Context, Hono } from "jsr:@hono/hono@4.6.15";
-
+import { HTML, tsxToJs } from "../../mini-webapp.ts";
 // $ deno run -A ../../serve.ts ./main.ts
 
 const app = new Hono();
-app.get("/", (ctx: Context) => {
-  return ctx.html("<h1>hello world</h1>");
+app.get("/", async (ctx: Context) => {
+  const code = await tsxToJs({
+    filename: "./client.tsx",
+    debug: true,
+    baseUrl: "/",
+  });
+  const html = HTML({ code, id: "app", title: "Counter" });
+  return ctx.html(html);
 });
 export default app;
