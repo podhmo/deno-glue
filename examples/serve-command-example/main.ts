f2c3a55f1a16da8a13c391d6e64783aaf2254375
podhmo <ababjam61+github@gmail.com>
2025-01-02 13:38:32 +0900

    chore: update examples

--- examples/serve-command-example/main.ts
+++ examples/serve-command-example/main.ts
@@ -1,7 +1,11 @@
 import { type Context, Hono } from "jsr:@hono/hono@4.6.15";
-import { HTML, tsxToJs } from "../../mini-webapp.ts";
+import { HTML, tsxToJs } from "jsr:@podhmo/glue@0.1.3/mini-webapp";
 
-// $ deno run -A ../../serve.ts ./main.ts
+// serve for development
+// $ deno run -A jsr:@podhmo/glue@0.1.3 serve --port 8080 ./main.ts
+//
+// bunle to single html file
+// $ deno run -A jsr:@podhmo/glue@0.1.3 bundle --output-style html --html-id app ./client.tsx > index.html
 
 const app = new Hono();
 app.get("/", async (ctx: Context) => {
