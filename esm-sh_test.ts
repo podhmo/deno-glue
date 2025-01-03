9454f1b9f5b88bfa5d117c8dd4fb953f3d8fc730
podhmo <ababjam61+github@gmail.com>
2025-01-03 18:26:27 +0900

    feat: add dependencies tracking

--- esm-sh_test.ts
+++ esm-sh_test.ts
@@ -8,10 +8,20 @@ Deno.test("loadConfig", async () => {
   );
 
   const want = {
-    "npm:@types/react@18": { pkg: "@types/react@18.3.18", suffix: "" },
+    "npm:@types/react@18": {
+      pkg: "@types/react@18.3.18",
+      suffix: "?deps=@types/prop-types@15.7.14,csstype@3.1.3",
+    },
     "npm:preact@10.5.13": { pkg: "preact@10.5.13", suffix: "" },
-    "npm:react-dom@18": { pkg: "react-dom@18.3.1", suffix: "?deps=react@18.3.1" },
-    "npm:react@18": { pkg: "react@18.3.1", suffix: "" },
+    "npm:react-dom@18": {
+      pkg: "react-dom@18.3.1",
+      suffix:
+        "?deps=js-tokens@4.0.0,loose-envify@1.4.0,react@18.3.1,scheduler@0.23.2",
+    },
+    "npm:react@18": {
+      pkg: "react@18.3.1",
+      suffix: "?deps=js-tokens@4.0.0,loose-envify@1.4.0",
+    },
     "preact": { pkg: "preact@10.5.13", suffix: "" },
     "@hono/hono": { pkg: "jsr/@hono/hono@4.6.15", suffix: "" },
   };
@@ -27,10 +37,20 @@ Deno.test("loadConfig with development", async () => {
   );
 
   const want = {
-    "npm:@types/react@18": { pkg: "@types/react@18.3.18", suffix: "?dev" },
+    "npm:@types/react@18": {
+      pkg: "@types/react@18.3.18",
+      suffix: "?dev&deps=@types/prop-types@15.7.14,csstype@3.1.3",
+    },
     "npm:preact@10.5.13": { pkg: "preact@10.5.13", suffix: "?dev" },
-    "npm:react-dom@18": { pkg: "react-dom@18.3.1", suffix: "?dev&deps=react@18.3.1" },
-    "npm:react@18": { pkg: "react@18.3.1", suffix: "?dev" },
+    "npm:react-dom@18": {
+      pkg: "react-dom@18.3.1",
+      suffix:
+        "?dev&deps=js-tokens@4.0.0,loose-envify@1.4.0,react@18.3.1,scheduler@0.23.2",
+    },
+    "npm:react@18": {
+      pkg: "react@18.3.1",
+      suffix: "?dev&deps=js-tokens@4.0.0,loose-envify@1.4.0",
+    },
     "preact": { pkg: "preact@10.5.13", suffix: "?dev" },
     "@hono/hono": { pkg: "jsr/@hono/hono@4.6.15", suffix: "?dev" },
   };
