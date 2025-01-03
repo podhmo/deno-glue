bffe056a68e7081a2ae53cad6e66c3cec1bcfb48
podhmo <ababjam61+github@gmail.com>
2025-01-04 08:26:56 +0900

    chore: update testdata/dst

--- testdata/dst/cards-component.stdout.js
+++ testdata/dst/cards-component.stdout.js
@@ -1,6 +1,6 @@
 // testdata/src/cards-component.tsx
 import { chunk } from "https://esm.sh/jsr/@std/collections@1.0.9";
-import { jsx } from "https://esm.sh/preact@10/jsx-runtime";
+import { jsx } from "https://esm.sh/preact@10.5.13/jsx-runtime";
 function CardsComponent(ns, size) {
   return /* @__PURE__ */ jsx("section", { class: "cards", children: chunk(ns, size).map((row, i) => /* @__PURE__ */ jsx("div", { style: { display: "flex" }, children: row.map((n) => /* @__PURE__ */ jsx("div", { style: { padding: "1rem" }, children: n }, n)) }, i)) });
 }
--- testdata/dst/hello-component.mjs
+++ testdata/dst/hello-component.mjs
@@ -1,5 +1,5 @@
 // testdata/src/hello-component.tsx
-import { jsxs } from "https://esm.sh/preact@10/jsx-runtime";
+import { jsxs } from "https://esm.sh/preact@10.5.13/jsx-runtime";
 function hello(name) {
   return /* @__PURE__ */ jsxs("p", { children: [
     "Hello ",
