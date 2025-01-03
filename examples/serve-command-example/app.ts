ec8e8f6837928e18ea080cca05616fc626714060
podhmo <ababjam61+github@gmail.com>
2025-01-04 08:39:04 +0900

    bump version

--- examples/serve-command-example/app.ts
+++ examples/serve-command-example/app.ts
@@ -1,13 +1,13 @@
 import { join as pathjoin } from "jsr:@std/path@1.0.8/join";
 
 import { type Context, Hono } from "jsr:@hono/hono@4.6.15";
-import { HTML, tsxToJs } from "jsr:@podhmo/glue@0.2.1/mini-webapp";
+import { HTML, tsxToJs } from "jsr:@podhmo/glue@0.2.2/mini-webapp";
 
 // serve for development
-// $ deno run -A jsr:@podhmo/glue@0.2.1 serve --port 8080 ./app.ts
+// $ deno run -A jsr:@podhmo/glue@0.2.2 serve --port 8080 ./app.ts
 //
 // bunle to single html file
-// $ deno run -A jsr:@podhmo/glue@0.2.1 bundle --output-style html --html-id app ./client.tsx > index.html
+// $ deno run -A jsr:@podhmo/glue@0.2.2 bundle --output-style html --html-id app ./client.tsx > index.html
 
 const app = new Hono();
 app.get("/", async (ctx: Context) => {
