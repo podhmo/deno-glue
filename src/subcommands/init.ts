c56b40ebda66f35d043e133f0156d66c5b7f1fb7
podhmo <ababjam61+github@gmail.com>
2025-01-06 19:52:14 +0900

    refactor: mv {init,bundle,serve} to src/subcommands

--- src/subcommands/init.ts
+++ src/subcommands/init.ts
@@ -29,12 +29,12 @@ export async function main(
   switch (options.template) {
     case "serve": {
       await copyFile({
-        from: "./examples/serve-command-example/client.tsx",
+        from: "./templates/serve/client.tsx",
         to: "./client.tsx",
         debug: options.debug,
       });
       await copyFile({
-        from: "./examples/serve-command-example/app.ts",
+        from: "./templates/serve/app.ts",
         to: "./app.ts",
         debug: options.debug,
       });
