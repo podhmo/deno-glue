92080abbafb40ff7a08e487c8d9fc3ef65fb9da4
podhmo <ababjam61+github@gmail.com>
2025-01-11 02:21:51 +0900

    chore: fix init template's version

--- src/subcommands/templates/serve/app.ts
+++ src/subcommands/templates/serve/app.ts
@@ -1,7 +1,7 @@
 import { join as pathjoin } from "jsr:@std/path@1.0.8/join";
 
 import { type Context, Hono } from "jsr:@hono/hono@4.6.15";
-import { HTML, tsxToJs } from "jsr:@podhmo/glue@0.2.2/mini-webapp";
+import { HTML, tsxToJs } from "jsr:@podhmo/glue@0.2.3/mini-webapp";
 
 // serve for development
 // $ deno run -A jsr:@podhmo/glue@0.2.2 serve --port 8080 ./app.ts
